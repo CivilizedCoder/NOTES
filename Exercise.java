cd4373b2c3297d2546838ffe114d7a13a5d75ab5
Timmymelon <130569519+Timmymelon@users.noreply.github.com>
2023-11-13 11:42:17 -0500

    Create Exercise.java

---
+++ Exercise.java
@@ -0,0 +1,49 @@
+
+public class Main {
+	
+	public static void main(String[]args) {
+		Main m = new Main();
+		m.go();
+		int[][] graphUndir = {
+			 //  A B C D E
+				{0,1,1,1,0,},
+				{1,0,0,0,1},
+				{1,0,0,1,0},
+				{1,0,1,0,1},
+				{1,0,1,0,1,}
+				};
+	
+				
+				
+				
+		int[][] graphDir = {
+				 //  A B C D E
+					{0,1,0,0,0},
+					{0,0,0,0,1},
+					{1,0,0,1,0},
+					{0,0,0,0,0},
+					{1,0,0,0,0}
+					};
+	}
+	
+	public void go() {
+		
+	}
+	
+	
+	public void pathDir() {
+		
+	}
+	
+	public void pathUndir() {
+		
+	}
+	
+	public void circDir() {
+		
+	}
+	public void circUndir() {
+		
+	}
+
+}
